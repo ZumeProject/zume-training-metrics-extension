5686a2589463cc2c286ee04a513ecf7978af809a
Chris Chasm <chris@chasm.solutions>
2021-02-12 10:21:37 -0700

    Added protection for export

--- formats/json-zume-training-registrations.php
+++ formats/json-zume-training-registrations.php
@@ -3,13 +3,15 @@
 /**
  * LOAD DATA TYPE FORMAT
  */
+if ( defined( 'ABSPATH' ) ) { // confirm wp is loaded
     class Zume_Training_JSON_Registrations
     {
 
         public $token = 'json_zume_training';
         public $label = 'JSON (Zume)';
 
-        public function format( $format ) {
+        public function format($format)
+        {
             /* Build base template of a format*/
             $format[$this->token] = get_dt_metrics_export_base_format();
 
@@ -35,19 +37,21 @@
             return $format;
         }
 
-        public function format_class( $classes) {
+        public function format_class($classes)
+        {
             $classes[$this->token] = __CLASS__;
             return $classes;
         }
 
-        public function create( $response ) {
+        public function create($response)
+        {
 
-            if ( ! isset( $response['type']['registrations'], $response['configuration'], $response['destination'] ) ){
+            if (!isset($response['type']['registrations'], $response['configuration'], $response['destination'])) {
                 return false;
             }
 
             $args = [
-                'timestamp' => current_time( 'Y-m-d H:i:s' ),
+                'timestamp' => current_time('Y-m-d H:i:s'),
                 'columns' => [],
                 'rows' => [],
                 'export' => $response,
@@ -58,13 +62,13 @@
             /**
              * Create results according to selected type
              */
-            if ( 'by_day' === $response['type']['registrations'] ) {
+            if ('by_day' === $response['type']['registrations']) {
                 $args['rows'] = $this->query_by_day();
-                $args['columns'] = array_keys( $args['rows'][0] );
+                $args['columns'] = array_keys($args['rows'][0]);
             }
 
             // kill if no results
-            if (empty( $args['rows'] )) {
+            if (empty($args['rows'])) {
                 echo '<div class="notice notice-warning is-dismissible">
                      <p>No results found for this configuration. Likely, there are no records for the countries you specified. Could not generate csv file.</p>
                  </div>';
@@ -72,55 +76,55 @@
             }
 
             // destination
-            $one_time_key = hash( 'sha256', get_current_user_id() . time() . dt_get_site_id() . rand( 0, 999 ) );
+            $one_time_key = hash('sha256', get_current_user_id() . time() . dt_get_site_id() . rand(0, 999));
             $postid = $response['configuration'];
-            switch ( $response['destination'] ) {
+            switch ($response['destination']) {
                 case 'expiring48':
-                    $args['link'] = esc_url( plugin_dir_url( __FILE__ ) ) . esc_url( basename( __FILE__ ) ) . '?expiring48=' . esc_attr( $one_time_key );
+                    $args['link'] = esc_url(plugin_dir_url(__FILE__)) . esc_url(basename(__FILE__)) . '?expiring48=' . esc_attr($one_time_key);
                     $args['key'] = $one_time_key;
-                    set_transient( 'metrics_exports_' . $one_time_key, $args, 60 . 60 . 48 );
+                    set_transient('metrics_exports_' . $one_time_key, $args, 60 . 60 . 48);
                     echo '<div class="notice notice-warning is-dismissible">
                              <p>
                                  Link expiring in 48 hours:<br>
-                                 <a href="' . esc_url( plugin_dir_url( __FILE__ ) ) . esc_url( basename( __FILE__ ) ) . '?expiring48=' . esc_attr( $one_time_key ) . '"
-                                 target="_blank">' . esc_url( plugin_dir_url( __FILE__ ) ) . esc_url( basename( __FILE__ ) ) . '?expiring48=' . esc_attr( $one_time_key ) . '
+                                 <a href="' . esc_url(plugin_dir_url(__FILE__)) . esc_url(basename(__FILE__)) . '?expiring48=' . esc_attr($one_time_key) . '"
+                                 target="_blank">' . esc_url(plugin_dir_url(__FILE__)) . esc_url(basename(__FILE__)) . '?expiring48=' . esc_attr($one_time_key) . '
                                  </a>
                              </p>
                          </div>';
                     break;
                 case 'expiring360':
-                    $args['link'] = esc_url( plugin_dir_url( __FILE__ ) ) . esc_url( basename( __FILE__ ) ) . '?expiring360=' . esc_attr( $one_time_key );
+                    $args['link'] = esc_url(plugin_dir_url(__FILE__)) . esc_url(basename(__FILE__)) . '?expiring360=' . esc_attr($one_time_key);
                     $args['key'] = $one_time_key;
-                    set_transient( 'metrics_exports_' . $one_time_key, $args, 60 . 60 . 360 );
+                    set_transient('metrics_exports_' . $one_time_key, $args, 60 . 60 . 360);
                     echo '<div class="notice notice-warning is-dismissible">
                              <p>
                                  Link expiring in 15 days:<br>
-                                 <a href="' . esc_url( plugin_dir_url( __FILE__ ) ) . esc_url( basename( __FILE__ ) ) . '?expiring360=' . esc_attr( $one_time_key ) . '"
-                                 target="_blank">' . esc_url( plugin_dir_url( __FILE__ ) ) . esc_url( basename( __FILE__ ) ) . '?expiring360=' . esc_attr( $one_time_key ) . '
+                                 <a href="' . esc_url(plugin_dir_url(__FILE__)) . esc_url(basename(__FILE__)) . '?expiring360=' . esc_attr($one_time_key) . '"
+                                 target="_blank">' . esc_url(plugin_dir_url(__FILE__)) . esc_url(basename(__FILE__)) . '?expiring360=' . esc_attr($one_time_key) . '
                                  </a>
                              </p>
                          </div>';
                     break;
                 case 'download':
-                    $args['link'] = esc_url( plugin_dir_url( __FILE__ ) ) . esc_url( basename( __FILE__ ) ) . '?download=' . esc_attr( $one_time_key );
+                    $args['link'] = esc_url(plugin_dir_url(__FILE__)) . esc_url(basename(__FILE__)) . '?download=' . esc_attr($one_time_key);
                     $args['key'] = $one_time_key;
-                    update_post_meta( $postid, 'download_' . $one_time_key, $args );
+                    update_post_meta($postid, 'download_' . $one_time_key, $args);
                     echo '<div class="notice notice-warning is-dismissible">
                              <p>
                                  One time download link:<br>
-                                 ' . esc_url( plugin_dir_url( __FILE__ ) ) . esc_url( basename( __FILE__ ) ) . '?download=' . esc_attr( $one_time_key ) . '
+                                 ' . esc_url(plugin_dir_url(__FILE__)) . esc_url(basename(__FILE__)) . '?download=' . esc_attr($one_time_key) . '
                              </p>
                          </div>';
                     break;
                 case 'permanent':
-                    $args['link'] = esc_url( plugin_dir_url( __FILE__ ) ) . esc_url( basename( __FILE__ ) ) . '?permanent=' . esc_attr( $one_time_key );
+                    $args['link'] = esc_url(plugin_dir_url(__FILE__)) . esc_url(basename(__FILE__)) . '?permanent=' . esc_attr($one_time_key);
                     $args['key'] = $one_time_key;
-                    update_post_meta( $postid, 'permanent_' . $one_time_key, $args );
+                    update_post_meta($postid, 'permanent_' . $one_time_key, $args);
                     echo '<div class="notice notice-warning is-dismissible">
                              <p>
                                  Permanent link (must be deleted manually):<br>
-                                 <a href="' . esc_url( plugin_dir_url( __FILE__ ) ) . esc_url( basename( __FILE__ ) ) . '?permanent=' . esc_attr( $one_time_key ) . '"
-                                 target="_blank">' . esc_url( plugin_dir_url( __FILE__ ) ) . esc_url( basename( __FILE__ ) ) . '?permanent=' . esc_attr( $one_time_key ) . '
+                                 <a href="' . esc_url(plugin_dir_url(__FILE__)) . esc_url(basename(__FILE__)) . '?permanent=' . esc_attr($one_time_key) . '"
+                                 target="_blank">' . esc_url(plugin_dir_url(__FILE__)) . esc_url(basename(__FILE__)) . '?permanent=' . esc_attr($one_time_key) . '
                                  </a>
                              </p>
                          </div>';
@@ -131,45 +135,47 @@
             return $response['configuration'] ?? 0; // return int config id, so ui reloads on same config
         }
 
-        public function update( $key, array $args ) {
-            if ( empty( $key ) ){
+        public function update($key, array $args)
+        {
+            if (empty($key)) {
                 return false;
             }
-            if ( ! isset( $args['timestamp'], $args['link'], $args['export'], $args['export']['configuration'], $args['export']['destination'], $args['export']['type']['registration'] ) ) {
+            if (!isset($args['timestamp'], $args['link'], $args['export'], $args['export']['configuration'], $args['export']['destination'], $args['export']['type']['registrations'])) {
                 return false;
             }
 
-            $args['timestamp'] = current_time( 'Y-m-d H:i:s' );
+            $args['timestamp'] = current_time('Y-m-d H:i:s');
 
             /**
              * Create results according to selected type
              */
-            if ( 'by_day' === $args['export']['type']['registrations'] ) {
+            if ('by_day' === $args['export']['type']['registrations']) {
                 $args['rows'] = $this->query_by_day();
-                $args['columns'] = array_keys( $args['rows'][0] );
+                $args['columns'] = array_keys($args['rows'][0]);
             }
 
             // update destination
             $postid = $args['export']['configuration'];
-            switch ( $args['export']['destination'] ) {
+            switch ($args['export']['destination']) {
                 case 'expiring48':
-                    set_transient( 'metrics_exports_' . $key, $args, 60 . 60 . 48 );
+                    set_transient('metrics_exports_' . $key, $args, 60 . 60 . 48);
                     break;
                 case 'expiring360':
-                    set_transient( 'metrics_exports_' . $key, $args, 60 . 60 . 360 );
+                    set_transient('metrics_exports_' . $key, $args, 60 . 60 . 360);
                     break;
                 case 'download':
-                    update_post_meta( $postid, 'download_' . $key, $args );
+                    update_post_meta($postid, 'download_' . $key, $args);
                     break;
                 case 'permanent':
-                    update_post_meta( $postid, 'permanent_' . $key, $args );
+                    update_post_meta($postid, 'permanent_' . $key, $args);
                     break;
             }
 
             return $args;
         }
 
-        public function query_by_day() {
+        public function query_by_day()
+        {
             global $wpdb;
             $results = $wpdb->get_results("
                     SELECT 
@@ -186,18 +192,24 @@
         }
 
         private static $_instance = null;
-        public static function instance() {
-            if (is_null( self::$_instance )) {
+
+        public static function instance()
+        {
+            if (is_null(self::$_instance)) {
                 self::$_instance = new self();
             }
             return self::$_instance;
         }
-        public function __construct() {
-            add_filter( 'dt_metrics_export_format', [ $this, 'format' ], 10, 1 );
-            add_filter( 'dt_metrics_export_register_format_class', [ $this, 'format_class' ], 10, 1 );
+
+        public function __construct()
+        {
+            add_filter('dt_metrics_export_format', [$this, 'format'], 10, 1);
+            add_filter('dt_metrics_export_register_format_class', [$this, 'format_class'], 10, 1);
         } // End __construct()
     }
+
     Zume_Training_JSON_Registrations::instance();
+}
 
 
 /**
@@ -207,6 +219,9 @@ if ( !defined( 'ABSPATH' )) {
 
     // @codingStandardsIgnoreLine
     require($_SERVER['DOCUMENT_ROOT'] . '/wp-load.php'); // loads the wp framework when called
+
+    dt_write_log('post wp-load');
+    dt_write_log(defined( 'ABSPATH' ));
 
     if ( isset( $_GET['expiring48'] ) || isset( $_GET['expiring360'] ) ) {
 
@@ -260,9 +275,14 @@ if ( !defined( 'ABSPATH' )) {
         }
 
         // refresh data
-        require_once( 'json-zume-training-registrations.php' );
-        $raw = maybe_unserialize( $raw );
-        $results = Zume_Training_JSON_Registrations::instance()->update( $token, $raw );
+            $raw = maybe_unserialize( $raw );
+        if (defined( 'ABSPATH' )){
+            require_once( 'json-zume-training-registrations.php' );
+            $results = Zume_Training_JSON_Registrations::instance()->update( $token, $raw );
+        } else {
+            $results = $raw;
+        }
+
 
         header( 'Content-type: application/json' );
 
